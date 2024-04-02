c5ec7a33f3cd4cd38fef7ab403b2a7294e8cebd0
183Patchanon <113663456+183Patchanon@users.noreply.github.com>
2024-04-02 12:44:54 +0700

    Add files via upload

--- appEncrypt.java
+++ appEncrypt.java
@@ -5,42 +5,14 @@ import java.security.NoSuchAlgorithmException;
 
 public class appEncrypt {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("n: ");
-        BigInteger n = sc.nextBigInteger();
-        sc.nextLine(); // newline character
-        System.out.print("path: ");
-        String path = sc.nextLine();
-
-        sc.close();
-        
-        findGCD fGCD = new findGCD();
-
-        // genPrime
-        BigInteger tmp = fGCD.GP.GenPrime(n, path);
-        // System.out.println(tmp);
-        
-        /* long result[] = fGCD.GenRandomNowithInverse(tmp);
-        for (int i = 0; i < 3; i++) {
-            System.out.println(result[i]);
-        } */
-        
-        // GCD
-        // System.out.println(fGCD.GCD(365, 1013));
-
-        // Inverse
-        // System.out.println(fGCD.findInverse(365, 1013));
-
         /* ******** Elgama ******** */
         System.out.println("******************Encrypt******************");
-        Egamal egm = new Egamal();
-        // System.out.println("Elgamal g " +egm.GenGenerator(tmp));
-        egm.ElgamalKeyGen(tmp);
-        System.out.println(egm.toString());
-        
-        // egm.ElgamalEncrypt("ascii.txt", "cipherText.txt");
+        Egamal encrypt = new Egamal(BigInteger.valueOf(229), BigInteger.valueOf(98), BigInteger.valueOf(563));
+        encrypt.ElgamalEncrypt("ascii.txt", "cipherText.txt");
 
         System.out.println("*****************Signature******************");
-        egm.ElgamalSignature("ascii.txt", "Signature.txt");
+        // egm.ElgamalSignature("ascii.txt", "Signature.txt");
+
+        // System.out.println(egm.RWHash("?ABCabcAAAAAAA".getBytes()));
     }
 }
